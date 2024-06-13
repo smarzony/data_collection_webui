d07e83f91b758da2e7867f78713cad3d99fbcd3e
automatycy <automatycy@swissrotors.com>
2024-06-13 09:30:15 +0200

    Working table display

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://automatycy:712394@/current_measure_deb?unix_socket=/run/mysqld/mysqld.sock'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def create_db_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='automatycy',
+            password='712394',
+            database='current_measure_deb',
+            unix_socket='/run/mysqld/mysqld.sock'
+        )
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
 
-db = SQLAlchemy(app)
-
-# Definicja modelu
-class AcquiredDataBackup(db.Model):
-    __tablename__ = 'acquired_data'
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, nullable=False)
-    mac = db.Column(db.String(50), nullable=False)
-    location = db.Column(db.String(100), nullable=True)
-    l1_value = db.Column(db.Float, nullable=False)
-    l2_value = db.Column(db.Float, nullable=False)
-    l3_value = db.Column(db.Float, nullable=False)
-
-def get_unique_mac_addresses():
-    return db.session.query(AcquiredDataBackup.mac).distinct().all()
-
-# Pobierz listę unikalnych adresów MAC wraz z lokalizacjami
 def get_unique_mac_addresses_with_locations():
-    return db.session.query(AcquiredDataBackup.mac, AcquiredDataBackup.location).distinct().all()
+    connection = create_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT DISTINCT mac, location FROM acquired_data")
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
 
 @app.route('/')
 def home():
@@ -37,25 +38,97 @@ def fetch_data():
     mac_address = request.args.get('mac_address')
     location = request.args.get('location')
 
-    # Pobierz dane z bazy danych zgodnie z przekazanymi parametrami
-    if mac_address and location:
-        data = AcquiredDataBackup.query.filter_by(mac=mac_address, location=location).order_by(AcquiredDataBackup.id.desc()).limit(num_records).all()
-    elif mac_address:
-        data = AcquiredDataBackup.query.filter_by(mac=mac_address).order_by(AcquiredDataBackup.id.desc()).limit(num_records).all()
-    elif location:
-        data = AcquiredDataBackup.query.filter_by(location=location).order_by(AcquiredDataBackup.id.desc()).limit(num_records).all()
-    else:
-        data = AcquiredDataBackup.query.order_by(AcquiredDataBackup.id.desc()).limit(num_records).all()
+    query = "SELECT * FROM acquired_data"
+    filters = []
+    params = []
 
-    # Przekształć dane na format JSON i zwróć jako odpowiedź
-    return jsonify([{
-        'time': entry.time.strftime('%Y-%m-%d %H:%M:%S'),
-        'mac': entry.mac,
-        'location': entry.location,
-        'l1_value': entry.l1_value,
-        'l2_value': entry.l2_value,
-        'l3_value': entry.l3_value
-    } for entry in data])
+    if mac_address:
+        filters.append("mac = %s")
+        params.append(mac_address)
+    if location:
+        filters.append("location = %s")
+        params.append(location)
+
+    if filters:
+        query += " WHERE " + " AND ".join(filters)
+    
+    query += " ORDER BY id DESC LIMIT %s"
+    params.append(num_records)
+
+    connection = create_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(query, params)
+    data = cursor.fetchall()
+    print('data: ', data)
+    cursor.close()
+    connection.close()
+
+    for entry in data:
+        entry['TIME'] = entry['TIME'].strftime('%Y-%m-%d %H:%M:%S')
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# from flask import Flask, render_template, jsonify, request
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://automatycy:712394@/current_measure_deb?unix_socket=/run/mysqld/mysqld.sock'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+# # Definicja modelu
+# class AcquiredData(db.Model):
+#     __tablename__ = 'acquired_data'
+#     id = db.Column(db.Integer, primary_key=True)
+#     time = db.Column(db.DateTime, nullable=False)
+#     mac = db.Column(db.String(50), nullable=False)
+#     location = db.Column(db.String(100), nullable=True)
+#     l1_value = db.Column(db.Float, nullable=False)
+#     l2_value = db.Column(db.Float, nullable=False)
+#     l3_value = db.Column(db.Float, nullable=False)
+
+# def get_unique_mac_addresses():
+#     return db.session.query(AcquiredData.mac).distinct().all()
+
+# # Pobierz listę unikalnych adresów MAC wraz z lokalizacjami
+# def get_unique_mac_addresses_with_locations():
+#     return db.session.query(AcquiredData.mac, AcquiredData.location).distinct().all()
+
+# @app.route('/')
+# def home():
+#     unique_mac_addresses_with_locations = get_unique_mac_addresses_with_locations()
+#     return render_template('index.html', unique_mac_addresses_with_locations=unique_mac_addresses_with_locations)
+
+# @app.route('/fetch_data')
+# def fetch_data():
+#     num_records = request.args.get('num_records', default=5, type=int)
+#     mac_address = request.args.get('mac_address')
+#     location = request.args.get('location')
+
+#     # Pobierz dane z bazy danych zgodnie z przekazanymi parametrami
+#     if mac_address and location:
+#         data = AcquiredData.query.filter_by(mac=mac_address, location=location).order_by(AcquiredData.id.desc()).limit(num_records).all()
+#     elif mac_address:
+#         data = AcquiredData.query.filter_by(mac=mac_address).order_by(AcquiredData.id.desc()).limit(num_records).all()
+#     elif location:
+#         data = AcquiredData.query.filter_by(location=location).order_by(AcquiredData.id.desc()).limit(num_records).all()
+#     else:
+#         data = AcquiredData.query.order_by(AcquiredData.id.desc()).limit(num_records).all()
+
+#     # Przekształć dane na format JSON i zwróć jako odpowiedź
+#     return jsonify([{
+#         'time': entry.time.strftime('%Y-%m-%d %H:%M:%S'),
+#         'mac': entry.mac,
+#         'location': entry.location,
+#         'l1_value': entry.l1_value,
+#         'l2_value': entry.l2_value,
+#         'l3_value': entry.l3_value
+#     } for entry in data])
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,59 +1,175 @@
-function fetchData(numRecords, selectedMacAddress, selectedLocation) {
-    $.ajax({
-        url: '/fetch_data',
-        type: 'GET',
-        data: { 
-            num_records: numRecords,
-            mac_address: selectedMacAddress,
-            location: selectedLocation
-        },
-        dataType: 'json',
-        success: function(data) {
-            var tbody = $('#data-container tbody');
-            tbody.empty();
-
-            var currentTime = new Date(); // Aktualny czas
-
-            data.forEach(function(entry) {
-                var recordTime = new Date(entry.time); // Czas utworzenia rekordu
-                var timeDifference = Math.round((currentTime - recordTime) / (1000 * 60)); // Różnica czasów w minutach
-
-                var row = '<tr>' +
-                    '<td>' + entry.time + '</td>' +
-                    '<td>' + timeDifference + ' minutes ago</td>' +
-                    '<td>' + entry.mac + '</td>' +
-                    '<td>' + entry.location + '</td>' +
-                    '<td>' + entry.l1_value + '</td>' +
-                    '<td>' + entry.l2_value + '</td>' +
-                    '<td>' + entry.l3_value + '</td>' +                    
-                    '</tr>';
-                tbody.append(row);
-            });
-        }
-    });
-}
-
 $(document).ready(function() {
-    var selectedMacAddress = $('#mac-address').val();
-    var selectedLocation = $('#location').val();
+    function drawChart(data) {
+        var labels = [];
+        var values = [];
 
-    // Wywołaj funkcję fetchData() po załadowaniu strony
-    var numRecords = $('#num-records').val();
-    fetchData(numRecords, selectedMacAddress, selectedLocation);
+        data.forEach(function(entry) {
+            labels.push(entry.time);
+            values.push(entry.l1_value);
+        });
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'L1 Value',
+                    data: values,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'minute'
+                        }
+                    }
+                }
+            }
+        });
+    }
 
     $('#record-form').submit(function(event) {
-        event.preventDefault(); // Zapobiega domyślnej akcji przesyłania formularza
+        event.preventDefault();
 
         var numRecords = $('#num-records').val();
-        selectedMacAddress = $('#mac-address').val();
-        selectedLocation = $('#location').val();
+        var macAddress = $('#mac-address').val();
+        var location = $('#location').val();
 
-        fetchData(numRecords, selectedMacAddress, selectedLocation);
+        fetchData(numRecords, macAddress, location);
     });
 
-    // Cyklicznie pobierz dane z formularza co 5 sekund
+    function fetchData(numRecords, macAddress, location) {
+        $.ajax({
+            url: '/fetch_data',
+            type: 'GET',
+            data: {
+                num_records: numRecords,
+                mac_address: macAddress,
+                location: location
+            },
+            dataType: 'json',
+            success: function(data) {
+                var tbody = $('#data-container tbody');
+                tbody.empty();
+
+                var currentTime = new Date();
+
+                data.forEach(function(entry) {
+                    var recordTime = new Date(entry.time);
+                    var timeDifference = Math.round((currentTime - recordTime) / (1000 * 60));
+                    var timeDifferenceText = '';
+                    
+                    if (timeDifference < 60) {
+                        timeDifferenceText = timeDifference + ' minutes ago';
+                    } else if (timeDifference < 1440) {
+                        timeDifferenceText = Math.floor(timeDifference / 60) + ' hours ago';
+                    } else {
+                        timeDifferenceText = Math.floor(timeDifference / 1440) + ' days ago';
+                    }
+
+                    var row = '<tr>' +
+                        '<td>' + entry.time + '</td>' +
+                        '<td>' + timeDifferenceText + '</td>' +
+                        '<td>' + entry.mac + '</td>' +
+                        '<td>' + entry.location + '</td>' +
+                        '<td>' + entry.l1_value + '</td>' +
+                        '<td>' + entry.l2_value + '</td>' +
+                        '<td>' + entry.l3_value + '</td>' +
+                        '</tr>';
+                    tbody.append(row);
+                });
+
+                drawChart(data);
+            }
+        });
+    }
+
+    // Fetch data on page load
+    $('#record-form').submit();
     setInterval(function() {
-        var numRecords = $('#num-records').val();
-        fetchData(numRecords, selectedMacAddress, selectedLocation);
+        $('#record-form').submit();
     }, 5000);
 });
+
+// function fetchData(numRecords, selectedMacAddress, selectedLocation) {
+//     $.ajax({
+//         url: '/fetch_data',
+//         type: 'GET',
+//         data: { 
+//             num_records: numRecords,
+//             mac_address: selectedMacAddress,
+//             location: selectedLocation
+//         },
+//         dataType: 'json',
+//         success: function(data) {
+//             var tbody = $('#data-container tbody');
+//             tbody.empty();
+
+//             var currentTime = new Date(); // Aktualny czas
+
+//             data.forEach(function(entry) {
+//                 var recordTime = new Date(entry.time); // Czas utworzenia rekordu
+//                 var timeDifference = Math.round((currentTime - recordTime) / (1000 * 60)); // Różnica czasów w minutach
+
+//                 var timeString;
+//                 if (timeDifference < 2) {
+//                     timeString = timeDifference + ' minute ago';
+//                 }
+//                 else if (timeDifference >= 2 & timeDifference < 60) {
+//                     timeString = timeDifference + ' minutes ago';
+//                 } else if (timeDifference >= 60 & timeDifference < 1440) {
+//                     var hours = Math.floor(timeDifference / 60);
+//                     var minutes = timeDifference % 60;
+//                     timeString = hours + ' hours, ' + minutes + ' minutes ago';
+//                 } else {
+//                     var days = Math.floor(timeDifference / 1440);
+//                     var remainingMinutes = timeDifference % 1440;
+//                     var hours = Math.floor(remainingMinutes / 60);
+//                     var minutes = remainingMinutes % 60;
+//                     timeString = days + ' days, ' + hours + ' hours, ' + minutes + ' minutes ago';
+//                 }
+
+//                 var row = '<tr>' +
+//                     '<td>' + entry.time + '</td>' +
+//                     '<td>' + timeString + '</td>' +
+//                     '<td>' + entry.mac + '</td>' +
+//                     '<td>' + entry.location + '</td>' +
+//                     '<td>' + entry.l1_value + '</td>' +
+//                     '<td>' + entry.l2_value + '</td>' +
+//                     '<td>' + entry.l3_value + '</td>' +                    
+//                     '</tr>';
+//                 tbody.append(row);
+//             });
+//         }
+//     });
+// }
+
+// $(document).ready(function() {
+//     var selectedMacAddress = $('#mac-address').val();
+//     var selectedLocation = $('#location').val();
+
+//     // Wywołaj funkcję fetchData() po załadowaniu strony
+//     var numRecords = $('#num-records').val();
+//     fetchData(numRecords, selectedMacAddress, selectedLocation);
+
+//     $('#record-form').submit(function(event) {
+//         event.preventDefault(); // Zapobiega domyślnej akcji przesyłania formularza
+
+//         var numRecords = $('#num-records').val();
+//         selectedMacAddress = $('#mac-address').val();
+//         selectedLocation = $('#location').val();
+
+//         fetchData(numRecords, selectedMacAddress, selectedLocation);
+//     });
+
+//     // Cyklicznie pobierz dane z formularza co 5 sekund
+//     setInterval(function() {
+//         var numRecords = $('#num-records').val();
+//         fetchData(numRecords, selectedMacAddress, selectedLocation);
+//     }, 5000);
+// });
