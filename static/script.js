7c4d8223cd6a13c894c8b866cf444d18f3ae20ff
smarzony <smarzony@gmail.com>
2024-06-18 15:03:10 +0200

    working chart with 3 phases

--- static/script.js
+++ static/script.js
@@ -9,8 +9,13 @@ $(document).ready(function() {
     let chartInstance = null;
 
     function drawChart(data) {
+        // Sortowanie danych od najstarszych do najnowszych
+        data.sort((a, b) => new Date(a[TIME_FIELD]) - new Date(b[TIME_FIELD]));
+
         const labels = data.map(entry => entry[TIME_FIELD]);
-        const values = data.map(entry => entry[L1_VALUE_FIELD]);
+        const l1Values = data.map(entry => entry[L1_VALUE_FIELD]);
+        const l2Values = data.map(entry => entry[L2_VALUE_FIELD]);
+        const l3Values = data.map(entry => entry[L3_VALUE_FIELD]);
 
         const ctx = document.getElementById('myChart').getContext('2d');
 
@@ -20,18 +25,32 @@ $(document).ready(function() {
         }
 
         console.log("Creating new chart instance with labels:", labels);
-        console.log("Creating new chart instance with values:", values);
+        console.log("Creating new chart instance with values:", l1Values, l2Values, l3Values);
 
         chartInstance = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: labels,
-                datasets: [{
-                    label: 'L1 Value',
-                    data: values,
-                    borderColor: 'rgb(75, 192, 192)',
-                    tension: 0.1
-                }]
+                datasets: [
+                    {
+                        label: 'L1 Value',
+                        data: l1Values,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'L2 Value',
+                        data: l2Values,
+                        borderColor: 'rgb(192, 75, 75)',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'L3 Value',
+                        data: l3Values,
+                        borderColor: 'rgb(75, 75, 192)',
+                        tension: 0.1
+                    }
+                ]
             },
             options: {
                 scales: {
@@ -120,119 +139,3 @@ $(document).ready(function() {
         $('#record-form').submit();
     }, 60*1000);
 });
-
-// $(document).ready(function() {
-//     const TIME_FIELD = 'TIME';
-//     const L1_VALUE_FIELD = 'L1_VALUE';
-//     let chartInstance = null; // Referencja do wykresu
-
-
-//     function drawChart(data) {
-//         const labels = data.map(entry => entry[TIME_FIELD]);
-//         const values = data.map(entry => entry[L1_VALUE_FIELD]);
-
-//         const ctx = document.getElementById('myChart').getContext('2d');
-
-//         // Sprawdź, czy istnieje już wykres, jeśli tak, zniszcz go
-//         if (chartInstance) {
-//             console.log("Destroying existing chart instance");
-//             chartInstance.destroy();
-//             chartInstance = null; // Ustaw na null po zniszczeniu
-//         }
-
-//         console.log("Creating new chart instance with labels:", labels);
-//         console.log("Creating new chart instance with values:", values);
-
-//         // Utwórz nowy wykres
-//         chartInstance = new Chart(ctx, {
-//             type: 'line',
-//             data: {
-//                 labels: labels,
-//                 datasets: [{
-//                     label: 'L1 Value',
-//                     data: values,
-//                     borderColor: 'rgb(75, 192, 192)',
-//                     tension: 0.1
-//                 }]
-//             },
-//             options: {
-//                 scales: {
-//                     x: {
-//                         type: 'time',
-//                         time: {
-//                             unit: 'minute'
-//                         },
-//                         adapters: {
-//                             date: {
-//                                 locale: dateFns
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         });
-//     }
-
-//     $('#record-form').submit(function(event) {
-//         event.preventDefault();
-
-//         var numRecords = $('#num-records').val();
-//         var macAddress = $('#mac-address').val();
-//         var location = $('#location').val();
-
-//         fetchData(numRecords, macAddress, location);
-//     });
-
-//     function fetchData(numRecords, macAddress, location) {
-//         $.ajax({
-//             url: '/fetch_data',
-//             type: 'GET',
-//             data: {
-//                 num_records: numRecords,
-//                 mac_address: macAddress,
-//                 location: location
-//             },
-//             dataType: 'json',
-//             success: function(data) {
-//                 console.log("Fetched data: ", data);
-//                 var tbody = $('#data-container tbody');
-//                 tbody.empty();
-
-//                 var currentTime = new Date();
-
-//                 data.forEach(function(entry) {
-//                     var recordTime = new Date(entry.TIME);
-//                     var timeDifference = Math.round((currentTime - recordTime) / (1000 * 60));
-//                     var timeDifferenceText = '';
-                    
-//                     if (timeDifference < 60) {
-//                         timeDifferenceText = timeDifference + ' minutes ago';
-//                     } else if (timeDifference < 1440) {
-//                         timeDifferenceText = Math.floor(timeDifference / 60) + ' hours ago';
-//                     } else {
-//                         timeDifferenceText = Math.floor(timeDifference / 1440) + ' days ago';
-//                     }
-
-//                     var row = '<tr>' +
-//                         '<td>' + entry.TIME + '</td>' +
-//                         '<td>' + timeDifferenceText + '</td>' +
-//                         '<td>' + entry.MAC + '</td>' +
-//                         '<td>' + entry.LOCATION + '</td>' +
-//                         '<td>' + entry.L1_VALUE + '</td>' +
-//                         '<td>' + entry.L2_VALUE + '</td>' +
-//                         '<td>' + entry.L3_VALUE + '</td>' +
-//                         '</tr>';
-//                     tbody.append(row);
-//                 });
-
-//                 drawChart(data);
-//             }
-//         });
-//     }
-
-//     // Fetch data on page load
-//     $('#record-form').submit();
-//     setInterval(function() {
-//         $('#record-form').submit();
-//     }, 5000);
-// });
