b5336827823c489039a81a9612be6029e1e1f292
smarzony <smarzony@gmail.com>
2024-06-18 10:27:41 +0200

    table, form working again, chart displaying with no data

--- static/script.js
+++ static/script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
+    const TIME_FIELD = 'TIME';
+    const L1_VALUE_FIELD = 'L1_VALUE';
+
+    
     function drawChart(data) {
         var labels = [];
         var values = [];
 
         data.forEach(function(entry) {
-            labels.push(entry.time);
+            labels.push(entry.TIME);
             values.push(entry.l1_value);
         });
 
@@ -54,13 +58,14 @@ $(document).ready(function() {
             },
             dataType: 'json',
             success: function(data) {
+                console.log("Fetched data: ", data);
                 var tbody = $('#data-container tbody');
                 tbody.empty();
 
                 var currentTime = new Date();
 
                 data.forEach(function(entry) {
-                    var recordTime = new Date(entry.time);
+                    var recordTime = new Date(entry.TIME);
                     var timeDifference = Math.round((currentTime - recordTime) / (1000 * 60));
                     var timeDifferenceText = '';
                     
@@ -73,13 +78,13 @@ $(document).ready(function() {
                     }
 
                     var row = '<tr>' +
-                        '<td>' + entry.time + '</td>' +
+                        '<td>' + entry.TIME + '</td>' +
                         '<td>' + timeDifferenceText + '</td>' +
-                        '<td>' + entry.mac + '</td>' +
-                        '<td>' + entry.location + '</td>' +
-                        '<td>' + entry.l1_value + '</td>' +
-                        '<td>' + entry.l2_value + '</td>' +
-                        '<td>' + entry.l3_value + '</td>' +
+                        '<td>' + entry.MAC + '</td>' +
+                        '<td>' + entry.LOCATION + '</td>' +
+                        '<td>' + entry.L1_VALUE + '</td>' +
+                        '<td>' + entry.L2_VALUE + '</td>' +
+                        '<td>' + entry.L3_VALUE + '</td>' +
                         '</tr>';
                     tbody.append(row);
                 });
@@ -95,81 +100,3 @@ $(document).ready(function() {
         $('#record-form').submit();
     }, 5000);
 });
-
-// function fetchData(numRecords, selectedMacAddress, selectedLocation) {
-//     $.ajax({
-//         url: '/fetch_data',
-//         type: 'GET',
-//         data: { 
-//             num_records: numRecords,
-//             mac_address: selectedMacAddress,
-//             location: selectedLocation
-//         },
-//         dataType: 'json',
-//         success: function(data) {
-//             var tbody = $('#data-container tbody');
-//             tbody.empty();
-
-//             var currentTime = new Date(); // Aktualny czas
-
-//             data.forEach(function(entry) {
-//                 var recordTime = new Date(entry.time); // Czas utworzenia rekordu
-//                 var timeDifference = Math.round((currentTime - recordTime) / (1000 * 60)); // Różnica czasów w minutach
-
-//                 var timeString;
-//                 if (timeDifference < 2) {
-//                     timeString = timeDifference + ' minute ago';
-//                 }
-//                 else if (timeDifference >= 2 & timeDifference < 60) {
-//                     timeString = timeDifference + ' minutes ago';
-//                 } else if (timeDifference >= 60 & timeDifference < 1440) {
-//                     var hours = Math.floor(timeDifference / 60);
-//                     var minutes = timeDifference % 60;
-//                     timeString = hours + ' hours, ' + minutes + ' minutes ago';
-//                 } else {
-//                     var days = Math.floor(timeDifference / 1440);
-//                     var remainingMinutes = timeDifference % 1440;
-//                     var hours = Math.floor(remainingMinutes / 60);
-//                     var minutes = remainingMinutes % 60;
-//                     timeString = days + ' days, ' + hours + ' hours, ' + minutes + ' minutes ago';
-//                 }
-
-//                 var row = '<tr>' +
-//                     '<td>' + entry.time + '</td>' +
-//                     '<td>' + timeString + '</td>' +
-//                     '<td>' + entry.mac + '</td>' +
-//                     '<td>' + entry.location + '</td>' +
-//                     '<td>' + entry.l1_value + '</td>' +
-//                     '<td>' + entry.l2_value + '</td>' +
-//                     '<td>' + entry.l3_value + '</td>' +                    
-//                     '</tr>';
-//                 tbody.append(row);
-//             });
-//         }
-//     });
-// }
-
-// $(document).ready(function() {
-//     var selectedMacAddress = $('#mac-address').val();
-//     var selectedLocation = $('#location').val();
-
-//     // Wywołaj funkcję fetchData() po załadowaniu strony
-//     var numRecords = $('#num-records').val();
-//     fetchData(numRecords, selectedMacAddress, selectedLocation);
-
-//     $('#record-form').submit(function(event) {
-//         event.preventDefault(); // Zapobiega domyślnej akcji przesyłania formularza
-
-//         var numRecords = $('#num-records').val();
-//         selectedMacAddress = $('#mac-address').val();
-//         selectedLocation = $('#location').val();
-
-//         fetchData(numRecords, selectedMacAddress, selectedLocation);
-//     });
-
-//     // Cyklicznie pobierz dane z formularza co 5 sekund
-//     setInterval(function() {
-//         var numRecords = $('#num-records').val();
-//         fetchData(numRecords, selectedMacAddress, selectedLocation);
-//     }, 5000);
-// });
